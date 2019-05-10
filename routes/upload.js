9ed3f56cc84441bc32c13364149b583c7465d16b
Admin <jaipkamat@gmail.com>
2019-05-10 15:51:50 -0700

    Retrieved datetime from uploaded filename

--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var multer = require('multer');
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const CardModel = require('../database/card').CardModel;
 
@@ -11,12 +11,18 @@ router.post('/', upload.single('prices'), function(req, res, next) {
     const string = req.file.buffer.toString(); // Convert the buffer to a string
     const cardArray = JSON.parse(string); // Convert the string to a list of cards
 
-    console.log(cardArray);
+    console.log(cardArray.length);
+
+    // Parse date from passed file for use in dateHistory array
+    const filename = req.file.originalname;
+    const removeExtension = filename.split('.')[0];
+    const unixDate = removeExtension.split('--')[1];
+    const unixDateNum = parseInt(unixDate);
+
+    const scrapeDateTime = new Date(unixDateNum);
 
     // For each JSON entry, create a new cardModel and persist it
 
-    // Parse date from passed file
-    // TODO
     // Map all data to new CardModel
     // TODO
     // Persist mapped data
