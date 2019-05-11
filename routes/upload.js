891e8377877f80cccfa5c0e9a3308e6fda24766f
Admin <jaipkamat@gmail.com>
2019-05-10 17:33:01 -0700

    Upload JSON not working as expected...need to find sustainable method

--- routes/upload.js
+++ routes/upload.js
@@ -3,42 +3,55 @@ const router = express.Router();
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 const CardModel = require('../database/card').CardModel;
+
+function parseFilenameToDate(filename) {
+    const removeExtension = filename.split('.')[0];
+    const unixDate = removeExtension.split('--')[1];
+    const unixDateNum = parseInt(unixDate);
+
+    return new Date(unixDateNum);
+}
+
+function modelDocumentFromCardData(card, date) {
+    return {
+        name: card.name,
+        setCode: card.setCode,
+        priceHistory: [
+            {
+                price1: Number(card.price1),
+                price1: Number(card.price2),
+                date: new Date(date)
+            }
+        ]
+    };
+}
 
 /* POST json to seed database */
 router.post('/', upload.single('prices'), function(req, res, next) {
     const string = req.file.buffer.toString(); // Convert the buffer to a string
     const cardArray = JSON.parse(string); // Convert the string to a list of cards
 
-    console.log(cardArray.length);
-
     // Parse date from passed file for use in dateHistory array
-    const filename = req.file.originalname;
-    const removeExtension = filename.split('.')[0];
-    const unixDate = removeExtension.split('--')[1];
-    const unixDateNum = parseInt(unixDate);
+    const scrapeDateTime = parseFilenameToDate(req.file.originalname);
 
-    const scrapeDateTime = new Date(unixDateNum);
+    console.log(scrapeDateTime);
 
-    // For each JSON entry, create a new cardModel and persist it
+    // Promise.all the following below
+    // Return findOne.then(if not found, create)
 
-    // Map all data to new CardModel
-    // TODO
-    // Persist mapped data
+    // const card = new CardModel({
+    //     name: 'Tester McKarnerson',
+    //     setCode: 'LOL',
+    //     priceHistory: []
+    // });
 
-    const card = new CardModel({
-        name: 'Tester McKarnerson',
-        setCode: 'LOL',
-        priceHistory: []
-    });
-
-    card.save()
-        .then(doc => {
-            console.log(doc);
-            console.log('card saved!');
-        })
-        .catch(console.log);
+    // card.save()
+    //     .then(doc => {
+    //         console.log(doc);
+    //         console.log('card saved!');
+    //     })
+    //     .catch(console.log);
 
     res.render('index', { title: 'Upload works!' });
 });
