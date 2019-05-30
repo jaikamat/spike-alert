9a73120df340891c5b603c92bdd747cb81fd1cd2
Admin <jaipkamat@gmail.com>
2019-05-30 12:15:00 -0700

    Searchbar component logs search to console

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
+    onSearchSubmit = arg => {
+        // Make GET request here
+        console.log(`${arg} logged to console`);
+    };
+
     render() {
         return (
-            <div>
-                <p>Front end is working</p>
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar userSearch={this.onSearchSubmit} />
             </div>
         );
     }
