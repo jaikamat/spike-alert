8733307a9c03f17adbe090a9dca91058788746e1
Admin <jaipkamat@gmail.com>
2019-05-30 13:59:44 -0700

    /search GET uses params to find cards

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const CardModel = require('../database/card').CardModel;
 
 /* GET search listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a card');
+router.get('/', async function(req, res, next) {
+    const name = req.query.name;
+    const cards = await CardModel.find({ name: name });
+
+    console.log(`User searched for ${name}`);
+
+    res.send(cards);
 });
 
 module.exports = router;
