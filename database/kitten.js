6c15af7a2aa9988a9cabaaa80fef9f14346d8fb2
Admin <jaipkamat@gmail.com>
2019-06-03 11:03:26 -0700

    Removed superagent dependency and old initial users boilerplate

--- database/kitten.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-let KittenSchema = new mongoose.Schema({
-    name: String
-});
-
-let Kitten = mongoose.model('Kitten', KittenSchema);
-
-module.exports.Kitten = Kitten;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const superagent = require('superagent');
-
-const Kitten = require('../database/kitten').Kitten;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    let ragnar = new Kitten({ name: 'Ragnar' });
-
-    ragnar.save().then(doc => {
-        console.log(doc);
-    });
-
-    res.send('respond with a resource');
+    res.send('respond with a User');
 });
 
 module.exports = router;
