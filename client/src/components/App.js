94f18641a441baac4e057347a3551fbdfcdbf07a
Admin <jaipkamat@gmail.com>
2019-06-10 16:08:50 -0700

    Created NavBar MVP, and moved to Semantic-UI-React. Closes #15 and #13

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import Home from './Home';
 import Search from './Search';
+import Login from './Login';
 import { Route, Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
 
 class App extends React.Component {
+    state = { activeItem: 'Home' };
+
+    handleClick = (e, el) => {
+        this.setState({ activeItem: el.name });
+    };
+
     render() {
+        const activeItem = this.state.activeItem;
+
         return (
             <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/search">Search</Link>
-                    </li>
-                </ul>
-
-                <Route exact path="/" component={Home} />
-                <Route path="/search" component={Search} />
-                <Route />
+                <Menu inverted fixed="top">
+                    <Menu.Item
+                        as={Link}
+                        to="/"
+                        name="Home"
+                        // position="left"
+                        onClick={this.handleClick}
+                        active={'Home' === activeItem}
+                    />
+                    <Menu.Menu position="right">
+                        <Menu.Item
+                            as={Link}
+                            to="/search"
+                            name="Search"
+                            // position="right"
+                            onClick={this.handleClick}
+                            active={'Search' === activeItem}
+                        />
+                        <Menu.Item
+                            as={Link}
+                            to="/login"
+                            name="Login"
+                            // position="right"
+                            onClick={this.handleClick}
+                            active={'Login' === activeItem}
+                        />
+                    </Menu.Menu>
+                </Menu>
+                <div style={{ marginTop: 50 }}>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/search" component={Search} />
+                    <Route path="/login" component={Login} />
+                    <Route />
+                </div>
             </div>
         );
     }
