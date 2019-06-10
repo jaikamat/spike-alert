5dad94dabb2dd4b5ff95d8f2ca5521a109992f7d
Admin <jaipkamat@gmail.com>
2019-06-10 16:08:50 -0700

    Created NavBar MVP, and moved to Semantic-UI-React. Closes #15. Closes #13.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Login = () => {
     return (
         <div>
-            <h1>This is home.</h1>
+            <h1>Login Here</h1>
         </div>
     );
 };
 
-export default Home;
+export default Login;
