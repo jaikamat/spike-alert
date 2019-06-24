eedf3a6f473fd9df855c25a6d9204180996a1815
Admin <jaipkamat@gmail.com>
2019-06-24 12:14:27 -0700

    Created custom user auth middleware

--- routes/user.js
+++ routes/user.js
@@ -2,32 +2,44 @@ const express = require('express');
 const router = express.Router();
 const UserModel = require('../database/user');
 
+// Custom auth middleware that checks for user present and
+// requesting user can only access their own data
+function isAuthenticated(req, res, next) {
+    if (!req.user) {
+        res.status(403).send('Not authorized');
+    } else if (req.params.userId !== req.user._id) {
+        res.status(403).send('Not authorized');
+    } else {
+        return next();
+    }
+}
+
 // Get user info
-router.get('/:userId', function(req, res) {
+router.get('/:userId', isAuthenticated, function(req, res) {
     return UserModel.getUserById(req.params.userId)
         .then(user => res.send(user))
-        .catch(err => console.log(error));
+        .catch(error => res.send(error));
 });
 
 // Add card to user card list
-router.post('/:userId/list/:cardId', function(req, res) {
+router.post('/:userId/list/:cardId', isAuthenticated, function(req, res) {
     UserModel.addCardToList(req.params.userId, req.params.cardId)
         .then(user => res.send(user))
-        .catch(error => console.log(error));
+        .catch(error => res.send(error));
 });
 
 // Get user card list
-router.get('/:userId/list', function(req, res) {
+router.get('/:userId/list', isAuthenticated, function(req, res) {
     UserModel.getUserList(req.params.userId)
         .then(list => res.send(list))
-        .catch(error => console.log(error));
+        .catch(error => res.send(error));
 });
 
 // Delete card from user card list
-router.put('/:userId/list/:cardId', function(req, res) {
+router.put('/:userId/list/:cardId', isAuthenticated, function(req, res) {
     UserModel.removeCardFromList(req.params.userId, req.params.cardId)
         .then(user => res.send(user))
-        .catch(error => console.log(error));
+        .catch(error => res.send(error));
 });
 
 module.exports = router;
