d4abbc83f707a88cf889a112a37f4ec9b3571357
Admin <jaipkamat@gmail.com>
2019-07-29 11:08:00 -0700

    Created winners/losers api route. References jaikamat/spike-alert-frontend#8

--- routes/search.js
+++ routes/search.js
@@ -18,4 +18,64 @@ router.get('/autocomplete', async function(req, res, next) {
     res.send(editedTitles);
 });
 
+router.get('/highlow', async function(req, res) {
+    const nonfoilProjection = {
+        name: 1,
+        setCode: 1,
+        setIcon: 1,
+        setName: 1,
+        'priceTrends.two_day.price1': 1,
+        'currentPrice.price1': 1
+    };
+
+    const foilProjection = {
+        name: 1,
+        setCode: 1,
+        setIcon: 1,
+        setName: 1,
+        'priceTrends.two_day.price2': 1,
+        'currentPrice.price2': 1
+    };
+
+    try {
+        const winnersNonfoil = await CardModel.find({}, nonfoilProjection)
+            .sort({ 'priceTrends.two_day.price1': -1 })
+            .limit(10);
+        const winnersFoil = await CardModel.find({}, foilProjection)
+            .sort({ 'priceTrends.two_day.price2': -1 })
+            .limit(10);
+        const losersNonfoil = await CardModel.find(
+            {
+                'priceTrends.two_day.price1': { $ne: null }
+            },
+            nonfoilProjection
+        )
+            .sort({ 'priceTrends.two_day.price1': 1 })
+            .limit(10);
+        const losersFoil = await CardModel.find(
+            // If sorted ascending, null values will be counted, so we need to exclude them
+            {
+                'priceTrends.two_day.price2': { $ne: null }
+            },
+            foilProjection
+        )
+            .sort({ 'priceTrends.two_day.price2': 1 })
+            .limit(10);
+
+        res.send({
+            foil: {
+                winners: winnersFoil,
+                losers: losersFoil
+            },
+            nonfoil: {
+                winners: winnersNonfoil,
+                losers: losersNonfoil
+            }
+        });
+    } catch (error) {
+        res.send(error);
+        console.log(error);
+    }
+});
+
 module.exports = router;
