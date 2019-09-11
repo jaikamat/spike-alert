1791913a345fc2ef44940f944ccdd855f65cb6be
Admin <jaipkamat@gmail.com>
2019-09-11 11:52:39 -0700

    Quick and dirty route to fetch scraped data from Google Cloud Function and persist

--- routes/upload.js
+++ routes/upload.js
@@ -49,4 +49,33 @@ router.post('/update-prices', function(req, res, next) {
         .catch(console.log);
 });
 
+/**
+ * Request new card data from Google Cloud Functions
+ */
+router.post('/gcf-update', function(req, res, next) {
+    req.setTimeout(1200000); // Set 20 min timeout
+
+    fetch('https://us-central1-robust-heaven-247118.cloudfunctions.net/scrape-cardsphere').then(
+        cards => {
+            const cardArray = cards;
+            const scrapeDateTime = new Date();
+
+            console.log(`Preparing to upsert ${cardArray.length} new cards...`);
+            console.log(`Scrape DateTime: ${scrapeDateTime}`);
+
+            return persistCards(cardArray, scrapeDateTime)
+                .then(info => {
+                    console.log(info);
+                    console.log(`Bulk write ${info.result.ok === 1 ? 'OK' : 'ERROR'}`);
+                    return cacheCardTitles();
+                })
+                .then(data => {
+                    console.log(`Cache was created with ${data.cache.length} titles`);
+                    res.render('index', { title: 'Upload works!' });
+                })
+                .catch(console.log);
+        }
+    );
+});
+
 module.exports = router;
