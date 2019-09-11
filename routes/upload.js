55b1a55f8f62123534b9d27c9a2ef1021d8af838
Admin <jaipkamat@gmail.com>
2019-09-11 13:02:54 -0700

    Ironed out data fetch and persistence route for cron tasking in Google Cloud

--- routes/upload.js
+++ routes/upload.js
@@ -5,6 +5,7 @@ const persistCards = require('../database/cardController').persistCards;
 const cacheCardTitles = require('../database/cardController').cacheCardTitles;
 const getBiggestGainers = require('../database/cardController').getBiggestGainers;
 const sendCardsSMS = require('../utils/send_sms');
+const fetch = require('node-fetch');
 
 /**
  * Posts JSON reports scraped from CS to the database
@@ -52,30 +53,41 @@ router.post('/update-prices', function(req, res, next) {
 /**
  * Request new card data from Google Cloud Functions
  */
-router.post('/gcf-update', function(req, res, next) {
-    req.setTimeout(1200000); // Set 20 min timeout
+router.post('/gcf-update', async function(req, res, next) {
+    // TODO: Break this up into separate services? This route does heavy lifting. It has to:
+    // - Wait for scrape data from Google Cloud Functions
+    // - Upsert all the card data
+    // - Cache all the card titles for autocorrect
+    // - Update all the price data
+    // ...which can take a considerable amount of time
 
-    fetch('https://us-central1-robust-heaven-247118.cloudfunctions.net/scrape-cardsphere').then(
-        cards => {
-            const cardArray = cards;
-            const scrapeDateTime = new Date();
+    req.setTimeout(1800000); // Set 30 min timeout
 
-            console.log(`Preparing to upsert ${cardArray.length} new cards...`);
-            console.log(`Scrape DateTime: ${scrapeDateTime}`);
+    try {
+        const cardData = await fetch(
+            'https://us-central1-robust-heaven-247118.cloudfunctions.net/scrape-cardsphere'
+        );
+        const cards = await cardData.json();
+        const scrapeDateTime = new Date();
 
-            return persistCards(cardArray, scrapeDateTime)
-                .then(info => {
-                    console.log(info);
-                    console.log(`Bulk write ${info.result.ok === 1 ? 'OK' : 'ERROR'}`);
-                    return cacheCardTitles();
-                })
-                .then(data => {
-                    console.log(`Cache was created with ${data.cache.length} titles`);
-                    res.render('index', { title: 'Upload works!' });
-                })
-                .catch(console.log);
-        }
-    );
+        console.log(`Preparing to upsert ${cards.length} new cards...`);
+        console.log(`Scrape DateTime: ${scrapeDateTime}`);
+
+        const info = await persistCards(cards, scrapeDateTime);
+        console.log(info);
+        console.log(`Bulk write ${info.result.ok === 1 ? 'OK' : 'ERROR'}`);
+
+        const cacheTitleData = await cacheCardTitles();
+        console.log(`Cache was created with ${cacheTitleData.cache.length} titles`);
+
+        const updatePriceMsg = await updatePriceTrends();
+        console.log(updatePriceMsg);
+
+        res.render('index', { title: 'Upload works!' });
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
 });
 
 module.exports = router;
